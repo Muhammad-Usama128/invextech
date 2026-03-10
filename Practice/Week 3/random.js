06fb4d45ad5fc98f056b128629d789650cedc8cb
Muhammad Usama <mudeveloper1@gmail.com>
2026-03-10 12:28:21 +0500

    Delete random.js

--- Practice/Week 3/random.js
+++
@@ -1,8 +0,0 @@
-//This file for show
-// how to use git stash
-// git stash save "message"  // to save the changes in stash with message
-// git stash list  // to see the list of stashes
-// git stash apply  // to apply the latest stash
-// git stash apply stash@{0}  // to apply a specific stash
-// git stash drop stash@{0}  // to delete a specific stash
-// git stash clear  // to delete all stashes
